9e1144231a38dcfd39d6b3150839304af9ca7dec
JanedoeGVA <xavier.costa@unige.ch>
2019-10-03 11:38:09 +0200

    adding plsql
    testing simple connection

--- src/main/java/metier/DB.java
+++ src/main/java/metier/DB.java
@@ -16,14 +16,18 @@ public class DB {
         Connection connect = null;
         try {
             // This will load the MySQL driver, each DB has its own driver
+            LOG.log(Level.INFO,"Opened database successfully");
+            LOG.log(Level.INFO,"Opened database successfully");
+            LOG.log(Level.INFO,"Opened database successfully");
             Class.forName("org.postgresql.Driver");
             // Setup the connection with the DB
             connect = DriverManager
                     .getConnection(System.getenv("POSTGRES_URL"),
                     System.getenv("POSTGRES_USER"), Utils.getProps(Constant.POSTGRES_PROPS, Constant.DB_PASS));
+            LOG.log(Level.INFO,"Opened database successfully");
         } catch (Exception e) {
             LOG.log(Level.SEVERE,e.getMessage());
         }
-        LOG.log(Level.INFO,"Opened database successfully");
+
     }
 }
